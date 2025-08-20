10346830760779c9b4675bdb304c6f306fa1065c
Dan Sully <dsully@users.noreply.github.com>
2025-08-20 07:18:34 -0700

    fix: GHA builds

--- flake.nix
+++ flake.nix
@@ -1,31 +1,67 @@
 {
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-    naersk.url = "github:nix-community/naersk";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs = {
+        systems.follows = "systems";
+      };
+    };
+
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
   };
 
   outputs = {
+    self,
     flake-utils,
     naersk,
     nixpkgs,
+    rust-overlay,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
+        overlays = [(import rust-overlay)];
+
         pkgs = (import nixpkgs) {
-          inherit system;
+          inherit system overlays;
         };
 
-        naersk' = pkgs.callPackage naersk {};
+        toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+        naersk' = pkgs.callPackage naersk {
+          cargo = toolchain;
+          clippy = toolchain;
+          rustc = toolchain;
+        };
       in {
         # For `nix build` & `nix run`:
-        defaultPackage = naersk'.buildPackage {
-          src = ./.;
+        packages = {
+          default = naersk'.buildPackage {
+            src = ./.;
+          };
+
+          clippy = naersk'.buildPackage {
+            src = ./.;
+            mode = "clippy";
+          };
         };
 
         # For `nix develop` (optional, can be skipped):
         devShell = pkgs.mkShell {
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
           nativeBuildInputs = with pkgs; [rustc cargo];
         };
       }
